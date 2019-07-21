827601ba715901bc9d575aa60d325dee44588471
Richard Brooks <rickbrooks22@gmail.com>
2019-07-21 00:04:38 -0400

    Added input statement and if_elif_else statements

--- ifElse.py
+++ ifElse.py
@@ -5,3 +5,14 @@ if name == 'Ricky':
     print("Hello Ricky!")
 else: 
     print("Hello Stranger!")
+
+value= input('What is your name?')
+if value == 'Rick':
+    print("Hello Rick!")
+elif value == 'Debbie':
+    print("Hello Debbie!")
+elif value == 'Kendall':
+    print('Hello Kendall!')
+else:
+    print("I'm sorry we don't recognize you %s" %value)
+
